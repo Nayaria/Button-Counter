b43443558fd10a762717cdce10bb3b3d54e18776
Naya Holzapfel <ny.holzapfel@gmx.de>
2024-01-25 13:57:04 +0100

    Started with JavaScript but I am a noob so its bad

---
+++ Script.js
@@ -0,0 +1,5 @@
+function CreateRoom(norbert) {
+    var baseurl="file:///Users/eholzapfel/Desktop/Button-Counter/"
+    window.location.href=window.location.href+"/?title="+document.getElementById("Creater").value
+    console.log(window.location.href)
+}
