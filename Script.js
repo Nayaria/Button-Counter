8972426428de84840a36ee2ad50a7f8dd9940a34
Naya Holzapfel <ny.holzapfel@gmx.de>
2024-01-26 08:59:02 +0100

    Um oops....

--- Script.js
+++
@@ -1,5 +0,0 @@
-function CreateRoom(norbert) {
-    var baseurl="file:///Users/eholzapfel/Desktop/Button-Counter/"
-    window.location.href=window.location.href+"/?title="+document.getElementById("Creater").value
-    console.log(window.location.href)
-}
