63e45329f3580ac828bfd97f519b7a9657fdf71a
Naya Holzapfel <ny.holzapfel@gmx.de>
2024-01-26 11:03:22 +0100

    Disabled everything except 0123456789

---
+++ Script.js
@@ -0,0 +1,6 @@
+document.querySelector("#Creater, #Joiner").addEventListener("keypress", function (evt) {
+    if (evt.which < 48 || evt.which > 57)
+    {
+        evt.preventDefault();
+    }
+});
