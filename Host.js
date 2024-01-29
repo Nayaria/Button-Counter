f3f59eb87ac32749fe138e22fabd891f997e2167
Naya Holzapfel <ny.holzapfel@gmx.de>
2024-01-29 10:35:47 +0100

    Title <h1> changes dynamically now based on room name

--- Host.js
+++ Host.js
@@ -1,11 +1,13 @@
-
 // when document is fully loaded....
 (() => {
     var storedTitle = localStorage.getItem('tempTitle');
     if (storedTitle) {
         document.title = storedTitle;
+        // Get the h1 element by its id
+        var roomNameElement = document.getElementById("RoomName");
+        // Change the text content of the h1 element
+        roomNameElement.textContent = storedTitle;
         // Clear the stored title in localStorage
-       localStorage.removeItem('tempTitle');
+        localStorage.removeItem('tempTitle');
     }
-}
-)()
+})();
