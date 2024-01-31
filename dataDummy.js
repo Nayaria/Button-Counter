cdd7bd531899dc348d1ef1fa83f94b8ee281cd41
Naya Holzapfel <ny.holzapfel@gmx.de>
2024-01-31 09:13:10 +0100

    extended clickamount counter to t

--- dataDummy.js
+++ dataDummy.js
@@ -3,24 +3,52 @@ var data = {
         {
             name: '10',
             tiles: {
-                A: {
-                    clickAmount: 0
-                },
-                B: {
-                    clickAmount: 0
-                }
-            }
+                A: { clickAmount: 0 },
+                B: { clickAmount: 0 },
+                C: { clickAmount: 0 },
+                D: { clickAmount: 0 },
+                E: { clickAmount: 0 },
+                F: { clickAmount: 0 },
+                G: { clickAmount: 0 },
+                H: { clickAmount: 0 },
+                I: { clickAmount: 0 },
+                J: { clickAmount: 0 },
+                K: { clickAmount: 0 },
+                L: { clickAmount: 0 },
+                M: { clickAmount: 0 },
+                N: { clickAmount: 0 },
+                O: { clickAmount: 0 },
+                P: { clickAmount: 0 },
+                Q: { clickAmount: 0 },
+                R: { clickAmount: 0 },
+                S: { clickAmount: 0 },
+                T: { clickAmount: 0 }
+            }    
         },
         {
             name: '100',
             tiles: {
-                A: {
-                    clickAmount: 0
-                },
-                B: {
-                    clickAmount: 0
-                }
-            }
+                A: { clickAmount: 0 },
+                B: { clickAmount: 0 },
+                C: { clickAmount: 0 },
+                D: { clickAmount: 0 },
+                E: { clickAmount: 0 },
+                F: { clickAmount: 0 },
+                G: { clickAmount: 0 },
+                H: { clickAmount: 0 },
+                I: { clickAmount: 0 },
+                J: { clickAmount: 0 },
+                K: { clickAmount: 0 },
+                L: { clickAmount: 0 },
+                M: { clickAmount: 0 },
+                N: { clickAmount: 0 },
+                O: { clickAmount: 0 },
+                P: { clickAmount: 0 },
+                Q: { clickAmount: 0 },
+                R: { clickAmount: 0 },
+                S: { clickAmount: 0 },
+                T: { clickAmount: 0 }
+            }            
         }
     ]
 }
